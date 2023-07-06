7b99440e014854751950470b34096ccebe42c858
Gopher <davidalexanders.pro@gmail.com>
2023-07-06 23:04:11 +0500

    the vagaries of the under-linter are resolved

--- piscine/piscine.go
+++ piscine/piscine.go
@@ -15,7 +15,7 @@ import (
 
 // PrintCombN prints all possible combinations of n different digits in ascending order.
 func PrintCombN(k int) {
-    const base = 10
+	const base = 10
 	var sres []string
 
 	res := combine(base, k)
@@ -23,14 +23,13 @@ func PrintCombN(k int) {
 	for _, arr := range res {
 		sres = append(sres, aggregator(arr))
 	}
-    
+
 	fmt.Printf("%v\n\n", trimArtefact(sres))
 }
 
 func aggregator(arr []int) string {
 	length := len(arr)
-	var bufString string 
-
+	var bufString string
 
 	for i := 0; i < length; i++ {
 		bufString += fmt.Sprintf("%v", arr[i])
@@ -61,29 +60,28 @@ func trimArtefact(s []string) (res string) {
 	return res
 }
 
-
 func combine(n int, k int) [][]int {
-    results := [][]int{}
-    if k > n {
-        return results
-    }
-    
-    dfs([]int{}, n, k, 0, &results)
-    return results
+	results := [][]int{}
+	if k > n {
+		return results
+	}
+
+	dfs([]int{}, n, k, 0, &results)
+	return results
 }
 
 func dfs(buf []int, n, k, idx int, results *[][]int) {
-    if k == 0 {
-        tmp := make([]int, len(buf))
-        copy(tmp, buf)
-        *results = append(*results, tmp)
-    }
-    
-    for i := idx; i <= n; i++ {
-        buf = append(buf, i)
-        
-        dfs(buf, n, k-1, i+1, results)
-        
-        buf = buf[:len(buf)-1]
-    }
+	if k == 0 {
+		tmp := make([]int, len(buf))
+		copy(tmp, buf)
+		*results = append(*results, tmp)
+	}
+
+	for i := idx; i <= n; i++ {
+		buf = append(buf, i)
+
+		dfs(buf, n, k-1, i+1, results)
+
+		buf = buf[:len(buf)-1]
+	}
 }
