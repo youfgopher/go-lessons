91d36596345a69690fdc514e9922e5ca0d3de2d2
Gopher <davidalexanders.pro@gmail.com>
2023-07-14 10:47:38 +0500

    added third example

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import examples "github.com/youfgopher/go-lessons/08_exam"
+
 func main() {
 	//println("print program name with prog args")
 	//args.PrintProgramName()
@@ -16,4 +18,7 @@ func main() {
 	//input: main 12345 -> 1+2+3+4+5 -> output: 15
 
 	// example 3
+	examples.PrintCombN()
+	// input: go run main.go 3
+	// output: 123 124 125 126 128 129 134 135 136 137 138 139 145 146 147 148 149 156...
 }
